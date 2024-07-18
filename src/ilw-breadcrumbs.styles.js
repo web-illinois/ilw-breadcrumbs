e87c3070d110d2e36bd5525dee1c9b0c16bfb851
Natalie Johnstone <nhj@illinois.edu>
2024-07-18 15:22:40 -0500

    rewrite of ilw-breadcrumbs component so it does not need ilw-breadcrumbs-page component

--- src/ilw-breadcrumbs.styles.js
+++ src/ilw-breadcrumbs.styles.js
@@ -5,6 +5,41 @@ export default css`
         counter-reset: breadcrumb 0;
         color: #252525;
     }
+    li {
+        display: inline;
+        color: #252525;
+        vertical-align: baseline;
+    }
+    .separator {
+        position: relative;
+        display: inline-block;
+        height: 8px;
+        width: 8px;
+        padding: 0 4px;
+    }
+    .separator svg {
+        position: relative;
+        display: block;
+        width: 100%;
+        height: 100%;
+        fill: var(--il-blue);
+    }
+    a {
+        color: var(--ilw-link--color);
+    }
+    a:visited {
+        color: var(--ilw-link--visited-color);
+    }
+    a:focus, a:hover {
+        color: var(--ilw-link--focused-color);
+    }
+    a:last-child {
+        text-decoration: none;
+    }
+    a:last-child:focus, a:last-child:hover {
+        color: #252525;
+        cursor: text;
+    }
     ol {
         list-style: none;
         margin: 0;
